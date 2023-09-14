03d74453d93224d3e23fcfb07e611986f5be4c81
Jordan Primas <jorprimas20@gmail.com>
2023-09-14 04:09:30 -0700

    Creating new habit card and remove button

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("create-habit-form")
 
 
-// 
+// Grab values from form inputs 
 document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', (e) => {
         e.preventDefault()
@@ -10,13 +10,33 @@ document.addEventListener('DOMContentLoaded', () => {
             description: e.target.new_habit_description.value,
             image: e.target.new_habit_image.value
         }
-        console.log(newHabitObject)
-
-
+        buildHabit(newHabitObject)
+        form.reset() 
     })
-
 })
 
+//Build new habit card and delete button and append to DOM
+function buildHabit(habit) {
+   let habitCard = document.createElement("div")
+   let habitName = document.createElement("h4")
+   let habitDescription = document.createElement("p")
+   let habitImage = document.createElement("img")
+   let removeBtn = document.createElement("btn")
+
+   habitName.className = "habbit_name"
+   habitDescription.className = "habbit_description"
+   habitImage.className = "new_img"
+   removeBtn.className = "remove_button"
+
+   habitName.innerText = habit.name
+   habitDescription.innerText = habit.description
+   habitImage.src = habit.image
+   removeBtn.innerText = "completed"
+
+   habitCard.append(habitName, habitDescription, habitImage, removeBtn)
+
+
+}
 
 
 
